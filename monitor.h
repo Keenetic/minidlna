3ae31088412e24fc9a6629eae3bd6d2db6940fdb
Gleb Smirnoff <glebius@FreeBSD.org>
2018-01-17 00:07:43 -0800

    Separate platform indepenent code into monitor.c, and move inotify(7) specific code to monitor_inotify.c. Both monitor_inotify.c and monitor_kqueue.c provide their functions for adding and removing watches. Prefix these functions with monitor_.

--- monitor.h
+++ monitor.h
@@ -12,13 +12,15 @@ static inline void start_inotify_thread(pthread_t *inotify_thread){};
 static inline void stop_inotify_thread(pthread_t *inotify_thread){};
 #endif
 
+int monitor_insert_file(const char *name, const char *path);
 int monitor_insert_directory(int fd, char *name, const char * path);
 int monitor_remove_file(const char * path);
 int monitor_remove_directory(int fd, const char * path);
 
 #if defined(HAVE_INOTIFY) || defined(HAVE_KQUEUE)
 #define HAVE_WATCH 1
-int add_watch(int, const char *);
+int monitor_add_watch(int, const char *);
+int monitor_remove_watch(int, const char *);
 #endif
 
 #ifdef HAVE_KQUEUE
